b7ee86a8654189648a14919a77d4d91f2c574cba
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2024-12-24 18:09:38 +0530

    Time: 27 ms (84.33%), Space: 51.4 MB (60.96%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,40 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-         
         Arrays.sort(nums);
-        int n = nums.length;
-        for(int i=0;i<n;i++){
-           if(i>0 && nums[i] == nums[i-1]) continue;
-            
+        
+     
+        for(int i=0;i<nums.length;i++){
             int target = -nums[i];
-            
-           twoSum(nums,i,result,target);
+           if(i > 0 && nums[i] == nums[i-1]){
+              continue;
+           }else {
+               towSum(nums,result,i,target);
+           }
         }
-        
         return result;
-        
     }
-    public static void twoSum(int [] nums,int index, List<List<Integer>>ans,int target){
-       
+    
+    public static void towSum(int [] nums, List<List<Integer>> ans ,int index, int target){
+        
         int left = index+1;
         int right = nums.length-1;
         
-        while(left<right){
-            int sum = nums[left]+nums[right];
-          if(sum== target){
+        while(left < right) {
+         int sum = nums[left] + nums[right];
+            
+            if(sum == target){
              ans.add(Arrays.asList(nums[index],nums[left],nums[right]));
-              while(left< right && nums[left]== nums[left+1]) left++;
-              while(left<right && nums[right] == nums[right-1]) right--;
-              
+              while(left<right && nums[left] == nums[left+1]) left++;
+               while(left<right && nums[right] == nums[right-1]) right--;
+                left++;
+                right--;
+            }else if(sum < target){
               left++;
-              right--;
-          }else if(sum < target){
-             left++;
-          }else {
-              right--;
-          }
+            }else {
+             right--;
+            }
         }
-
     }
 }
